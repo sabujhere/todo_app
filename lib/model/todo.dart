04d2310c0fd319c262a896d2c12e82cd67a80866
Sabuj Saha <sabujece@gmail.com>
2019-01-02 21:48:42 -0600

    add end of statement

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -6,7 +6,7 @@ class Todo{
   int _priority;
 
   Todo(this._title, this._priority, this._date, [this._description]);
-  Todo.withId(this._id,this._title,this._priority, this._date, [this._description])
+  Todo.withId(this._id,this._title,this._priority, this._date, [this._description]);
   int get id => _id;
   String get title => _title;
   String get description => _description;
